e23c43b468eeef083de3ba163a1c04432cd7a55a
mlg <miicalucia95@gmail.com>
2023-08-28 14:58:21 -0300

    navbartoggle

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    console.log("abriste el menu")
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
