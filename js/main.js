ffb3801802f8eaee92c0dfc4efb3d73b7cd8a048
mlg <miicalucia95@gmail.com>
2023-09-04 12:43:40 -0300

    retoque en el navbar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const items = document.querySelector("#items");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
 
 cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+items.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
